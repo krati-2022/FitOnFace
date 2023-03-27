77ea90683277736e211b093af4d7b3d39be17906
Krati <pr955213@gmail.com>
2023-03-27 17:06:28 +0530

    First commit

---
+++ src/app/SharedComponent/shared-component/shared-component.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SharedComponentRoutingModule } from './shared-component-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SharedComponentRoutingModule
+  ]
+})
+export class SharedComponentModule { }
